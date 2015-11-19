fe7f058697057cdd4891910105424d618fa49a03
samjhooker <samjhooker@gmail.com>
2015-11-20 12:04:56 +1300

    deployed to web, integrated cores, ready to start with backend

--- Samdos/App_Start/WebApiConfig.cs
+++ Samdos/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Samdos
 {
@@ -10,6 +11,11 @@ namespace Samdos
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
+            // The below 2 lines states that any origin can access our API
+            // For more info: 
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
