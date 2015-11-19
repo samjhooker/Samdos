743ea0e20d1b3a60bcc62d67c5b3969e1651df0f
samjhooker <samjhooker@gmail.com>
2015-11-20 12:14:11 +1300

    added Testimonial object and Javascript to support it

---
+++ Samdos/Scripts/TestimonialModule.js
@@ -0,0 +1,46 @@
+﻿var TestimonialModule = (function () {
+
+    return {
+        getTestimonials: function (callback) {
+            $.ajax({ 
+                type: "GET",
+                dataType: "json",
+                // API url here
+                url: "http://samdos.azurewebsites.net/api/Testimonials",
+                success: function(data){        
+                    console.log(data);
+                    callback(data);
+                }
+            });
+        },
+
+        getTestimonialById: function (id, callback){
+            
+            $.ajax({ 
+                type: "GET",
+                dataType: "json",
+                url: "http://samdos.azurewebsites.net/api/Testimonials/" + id,
+                success: function(data){        
+                    console.log(data);
+                    callback(data);
+                }
+            });
+
+        },
+
+        addTestimonial: function (student, callback) {
+             
+            $.ajax({
+                url: "http://samdos.azurewebsites.net/api/Testimonials/",
+                type: "POST",
+                data : student,
+                success: function(data, textStatus, jqXHR)
+                {
+                    callback();
+                }
+            });
+
+        }
+    };
+
+}());
---
+++ Samdos/Scripts/testimonialScript.js
@@ -0,0 +1,43 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    console.log("testimonial page loaded");
+    loadTestimonials();
+});
+
+
+function loadTestimonials() {
+
+    var testimonialTable = document.getElementById("testimonialsGoHere");
+
+    TestimonialModule.getTestimonials(function (testimonialList) {
+        setupTestimonials(testimonialList);
+    });
+
+    function setupTestimonials(testimonials) {
+
+        output.log("finished pulling from the server");
+
+        // Loop through list of testimonials
+        for (i = 0; i < testimonials.length; i++) {
+
+            // Create row
+            var row = document.createElement('tr');
+            row.setAttribute("data-id", testimonials[i].ID);
+
+            // Create columns
+            var nameCol = document.createElement('td');
+            nameCol.innerHTML = testimonials[i].Name;
+            row.appendChild(nameCol);
+
+            var commentCol = document.createElement('td');
+            commentCol.innerHTML = testimonials[i].Comment;
+            row.appendChild(commentCol);
+
+            var replyCol = document.createElement('td');
+            replyCol.innerHTML = testimonials[i].Reply;
+            row.appendChild(replyCol);
+          
+            // Add newly created row to the table
+            testimonialTable.appendChild(row);
+        }
+    }
+}
