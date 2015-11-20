dad85b5d43dde1755145f672f31f1a3ba30218d7
samjhooker <samjhooker@gmail.com>
2015-11-20 15:27:57 +1300

    tried to fix, same errors

--- Samdos/Models/DataContext.cs
+++ Samdos/Models/DataContext.cs
@@ -1,28 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
-using System.Data.Entity;
-using System.Data.Entity.ModelConfiguration.Conventions;
-using System.Data.Entity.Migrations;
-using Samdos.Models;
 
 namespace Samdos.Models
 {
-
     [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
     public class DataContext : DbContext
     {
-
-        public DataContext() : base("name=DataContext")
-        {
-            if (!Database.Exists("DataContext"))
-            {
-                // Need to initialize database with configuration 
-                Database.SetInitializer(new MigrateDatabaseToLatestVersion<DataContext, MyConfiguration>());
-            }
-         }
-
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
 
         public class MyConfiguration : DbMigrationsConfiguration<DataContext>
         {
@@ -33,13 +26,17 @@ namespace Samdos.Models
             }
         }
 
-
-        public DbSet<Testimonial> Testimonials { get; set; }
-
-
-        /*protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        public DataContext()
+            : base("name=DataContext")
         {
-            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-        }*/
+            if (!Database.Exists("DataContext"))
+            {
+                // Need to initialize database with configuration 
+                Database.SetInitializer(new MigrateDatabaseToLatestVersion<DataContext, MyConfiguration>());
+            }
+        }
+
+        public System.Data.Entity.DbSet<Testimonial> Testimonials { get; set; }
+
     }
 }
