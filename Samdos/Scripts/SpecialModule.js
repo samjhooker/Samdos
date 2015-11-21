dd2f65c384d71881c84ddfecd38878e879642687
samjhooker <samjhooker@gmail.com>
2015-11-21 18:17:18 +1300

    created module and script to handle api loading.
    API now is called and grid is populated upon opeing

---
+++ Samdos/Scripts/SpecialModule.js
@@ -0,0 +1,69 @@
+﻿var SpecialModule = (function () {
+
+    return {
+        getSpecials: function (callback) {
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                // API url here
+                url: "http://samdos.azurewebsites.net/api/Specials/",
+                success: function (data) {
+                    console.log(data);
+                    callback(data);
+                }
+            });
+        },
+
+        getSpecialById: function (id, callback) {
+
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: "http://samdos.azurewebsites.net/api/Specials/" + id,
+                success: function (data) {
+                    console.log(data);
+                    callback(data);
+                }
+            });
+
+        },
+
+        addSpecial: function (student, callback) {
+
+            $.ajax({
+                url: "http://samdos.azurewebsites.net/api/Specials/",
+                type: "POST",
+                data: student,
+                success: function (data, textStatus, jqXHR) {
+                    callback();
+                }
+            });
+
+        },
+
+        updateSpecial: function (studentid, student, callback) {
+
+            $.ajax({
+                url: "http://samdos.azurewebsites.net/api/Specials/" + studentid,
+                type: "PUT",
+                data: student,
+                success: function (data, textStatus, jqXHR) {
+                    callback();
+                }
+            });
+        },
+
+        deleteSpecial: function (studentid, callback) {
+
+            $.ajax({
+                type: "DELETE",
+                dataType: "json",
+                url: "http://samdos.azurewebsites.net/api/Specials/" + studentid,
+                success: function (data) {
+                    callback();
+                }
+            });
+        }
+    };
+
+}());
---
+++ Samdos/Scripts/specialScript.js
@@ -0,0 +1,99 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    console.log("special page loaded");
+    loadTestimonials();
+});
+
+
+function loadTestimonials() {
+
+    var specialsGrid = document.getElementById("specialsGoHere");
+
+    SpecialModule.getSpecials(function (specialList) {
+        setupSpecials(specialList);
+    });
+
+    function setupSpecials(specials) {
+
+        console.log("finished pulling from the server");
+
+        // Loop through list of testimonials
+        for (i = 0; i < specials.length; i++) {
+
+            //create a special item
+            var specialItem = document.createElement("div");
+            specialItem.className += "special-item";
+
+            //create image for within item
+            var image = document.createElement("img");
+            image.className += "special-image";
+            image.src = specials[i].ImgURL;
+            specialItem.appendChild(image);
+
+            //create main body
+            var body = document.createElement("div");
+            body.className += "special-item-content";
+
+            //create the stuff inside the body
+            var header = document.createElement("h3");
+            header.textContent = specials[i].Title;
+            var text = document.createElement("p");
+            text.textContent = specials[i].Description;
+
+            var redeemButton = document.createElement("button");
+            redeemButton.className += "btn btn-primary light-shadow";
+            redeemButton.textContent = "Redeem Special";
+            redeemButton.onclick = function () {
+                window.location.href = "order.html";
+            };
+
+            var deleteButton = document.createElement("button");
+            deleteButton.className += "ghostButton";
+            deleteButton.textContent = "x";
+            deleteButton.onclick = function () { };
+
+
+
+
+            body.appendChild(header);
+            body.appendChild(text);
+            body.appendChild(redeemButton);
+            body.appendChild(deleteButton);
+
+
+            specialItem.appendChild(body);
+
+            specialsGrid.appendChild(specialItem);
+
+        }
+    }
+}
+
+
+
+
+function submitTestimonialButtonPressed() {
+
+    console.log("submit button pressed");
+
+    var name = document.getElementById("nameTextField").value;
+    var comment = document.getElementById("commentTextField").value;
+
+    if (name == "" || comment == "") {
+        window.alert("Both name and Comment required to proceed");
+    } else {
+        //post that object
+        var newTestamonial = {
+            ID: Math.floor((Math.random() * 9999999) + 1),
+            Name: name,
+            Comment: comment,
+            reply: "-"
+        }
+
+        TestimonialModule.addTestimonial(newTestamonial, function () {
+            window.location.href = "testimonials.html";
+        });
+
+    }
+
+
+}
